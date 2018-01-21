02bac8bf29784a8d444d13781e9302c9e196a24a
Lukasz Jaskiewicz <lukasz@jaskiewicz.de>
2018-01-21 22:23:15 +0100

    changed convert method call sample comments

--- samples/DDDN.OdtToHtml.Samples/Controllers/HomeController.cs
+++ samples/DDDN.OdtToHtml.Samples/Controllers/HomeController.cs
@@ -23,36 +23,37 @@ namespace DDDN.OdtToHtml.Samples
 			// subdirectory name where the ODT document content files like images will be stored
 			const string contentSubDirname = "content";
 
-			OdtConvertedData convertData = null;
+			// the structure which converted data which the Convert method returns
+			OdtConvertedData convertedData = null;
 
 			// generating path to the ODT document that will be converted
 			var odtFileInfo = _hostingEnvironment.WebRootFileProvider.GetFileInfo(Path.Combine("odt", id));
 
-			// open the ODT file from the file system
+			// settings for the Convert method
+			var odtConvertSettings = new OdtConvertSettings
+			{
+				RootElementTagName = "article", // root HTML tag info that will contains the converted HTML
+				RootElementId = "article_id", // the id attribute value of the root HTML tag
+				RootElementClassNames = "article_class", // the class attribute value of the root HTML tag
+				LinkUrlPrefix = $"/{contentSubDirname}", // here you can provide a prefix for all content links to match your environment requirements
+				DefaultTabSize = "2rem" // the default value for tabs (not tab stops)
+			};
+
+			// open the ODT document from the file system and call the Convert method to get the HTML/CSS
 			using (IOdtFile odtFile = new OdtFile(odtFileInfo.PhysicalPath))
 			{
-				var odtCon = new OdtConvert();
-
-				// CALL TO THE CONVERT METHOD
-				convertData = odtCon.Convert(odtFile, new OdtConvertSettings
-				{
-					RootElementTagName = "article", // root HTML tag info that will contains the converted HTML
-					RootElementId = "article_id", // the id attribute value of the root HTML tag
-					RootElementClassNames = "article_class", // the class attribute value of the root HTML tag
-					LinkUrlPrefix = $"/{contentSubDirname}", // here you can provide a prefix for all content links to match your environment requirements
-					DefaultTabSize = "2rem" // the default value for tabs (not tab stops)
-				});
+				convertedData = new OdtConvert().Convert(odtFile, odtConvertSettings);
 			}
 
-			ViewData["ArticleHtml"] = convertData.Html; // move the generated HTML to the razor view
-			ViewData["ArticleCss"] = convertData.Css; // move the generated CSS to the razor view
-			var usedFontFamilies = convertData.UsedFontFamilies; // all font families used in CSS/HTML useful for font links
-			var pageInfo = convertData.PageInfo; // contains dimensions and margins of the document sheet
-			var documentFirstHeader = convertData.DocumentFirstHeader; // the "text only" content of the first document header for preview purposes
-			var documentFirstParagraph = convertData.DocumentFirstParagraph; // the "text only" content of the first document paragraph for preview purposes
+			ViewData["ArticleHtml"] = convertedData.Html; // move the generated HTML to the razor view
+			ViewData["ArticleCss"] = convertedData.Css; // move the generated CSS to the razor view
+			var usedFontFamilies = convertedData.UsedFontFamilies; // all font families used in CSS/HTML useful for font links
+			var pageInfo = convertedData.PageInfo; // contains the document sheet's dimensions and margins
+			var documentFirstHeader = convertedData.DocumentFirstHeader; // the "text only" content of the first document header for preview purposes
+			var documentFirstParagraph = convertedData.DocumentFirstParagraph; // the "text only" content of the first document paragraph for preview purposes
 
 			// write the content (like images) of the ODT document to the file system to make it available to the web browser
-			foreach (var articleContent in convertData.EmbedContent)
+			foreach (var articleContent in convertedData.EmbedContent)
 			{
 				var contentLink = Path.Combine(_hostingEnvironment.WebRootPath, contentSubDirname, articleContent.LinkName);
 				System.IO.File.WriteAllBytes(contentLink, articleContent.Data);
